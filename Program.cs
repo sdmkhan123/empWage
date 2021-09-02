fd1b740890b92ad08a20a5a45e6a14194b5f2661
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-02 19:19:07 +0530

    Daily emp wage

--- Program.cs
+++ Program.cs
@@ -9,14 +9,18 @@ namespace empWage
             int empPresent = 1;
             Random random = new Random();
             int EmpCheck = random.Next(0, 2);
+            int empHr;
+            int empRatePerHr = 20;
             if (EmpCheck == empPresent)
             {
-                Console.WriteLine("Employee Present");
+                empHr = 8;
             }
             else
             {
-                Console.WriteLine("Employee Absent");
+                empHr = 0;
             }
+            int totalWage = empRatePerHr * empHr;
+            Console.WriteLine("Total wage is " + totalWage);
         }
     }
 }
