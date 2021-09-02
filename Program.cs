6f699967533cd1e7fae62d896b282155acd35014
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-02 20:06:40 +0530

    solving emp wage using switch case

--- Program.cs
+++ Program.cs
@@ -4,25 +4,26 @@ namespace empWage
 {
     class Program
     {
+        public const int fullTimePresent = 1;
+        public const int partTimePresent = 2;
+        public const int empRatePerHr = 20;
         static void Main(string[] args)
         {
-            int fullTimePresent = 1;
-            int partTimePresent = 2;
             Random random = new Random();
             int EmpCheck = random.Next(0, 3);
             int empHr;
             int empRatePerHr = 20;
-            if (EmpCheck == fullTimePresent)
+            switch (EmpCheck)
             {
-                empHr = 16;
-            }
-            else if (EmpCheck == partTimePresent)
-            {
-                empHr = 8;
-            }
-            else
-            {
-                empHr = 0;
+                case fullTimePresent:
+                    empHr = 8;
+                    break;
+                case partTimePresent:
+                    empHr = 4;
+                    break;
+                default:
+                    empHr = 0;
+                    break;
             }
             int totalWage = empRatePerHr * empHr;
             Console.WriteLine("Total wage is " + totalWage);
